77d510e91d0502b39e6ac2f983493c1b176e6359
cuibap_cuimia02@yahoo.com.vn <duydt2208@gmail.com>
2018-10-26 22:47:41 +0700

    add style, update html

--- task-2/js/main.js
+++ task-2/js/main.js
@@ -1,5 +1,8 @@
 function Main() {
     var githubService = new GitHubService();
+    var btnChangeOrder = document.getElementById('btnChangeOrder');
+    var defaultImg = 'https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png';
+
     this.onInit = function () {
         githubService.getFunchalSubscriptions(loadDataToContainer);
     }
@@ -7,20 +10,30 @@ function Main() {
     function loadDataToContainer() {
         var data = JSON.parse(this.responseText);
         var container = document.getElementById('container');
+        var template = getHTMLContent(data);
+
+        container.innerHTML = template;
+    }
+    
+    function getItemHref(item) {
+        return item.homepage ? 'title="View home page" href="' + item.homepage + '"' : '';
+    }
+
+    function getHTMLContent(data) {
         var template = '';
         for (var i = 0; i < data.length; i++) {
             var item = data[i];
-            template += '<a class="card" href="' + item.homepage + '" target="_blank">'
-            template += '<div class="image-container"><img src="https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png" /></div>'
-            template += '<div class="name">' + item.name + '</div>';
-            template += '<div class="full-name">' + item.full_name + '</div>';
-            template += '</a>'
+            template += '<li class="card"><a ' + getItemHref(item) + ' target="_blank">'
+            template += '<header class="image-container"><img src="' + defaultImg + '" /></header>'
+            template += '<footer><p class="name"><b>' + item.name + '</b></p>';
+            template += '<p class="full-name"><i>' + item.full_name + '</i></p></footer>';
+            template += '</a></li>'
         }
-        container.innerHTML = template;
+        return template;
     }
 }
 
-var main = new Main();
 window.onload = function () {
+    var main = new Main();
     main.onInit();
 }
